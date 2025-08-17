e68b24c3f97630724d501cfb6e03bdf176fc1c77
Gwenn Durpoix-Espinasson <durpoixgwenn@gmail.com>
2025-08-17 12:15:18 +0200

    add config file and health route

--- app/app.py
+++ app/app.py
@@ -1,17 +1,26 @@
 from flask import Flask
+from sqlalchemy.exc import OperationalError
+
 from models import db
 from routes import pos as pos_bp
+from health import health_bp
+import config
 
 def create_app():
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://admin:adminpass@localhost:5432/paylead_db"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    app.config.from_object(config.Config)
 
     db.init_app(app)
     app.register_blueprint(pos_bp)
+    app.register_blueprint(health_bp)
 
     with app.app_context():
-        db.create_all()
+        try:
+            db.create_all()
+        except OperationalError as e:
+            print(f"Database connection error: {e}")
+            raise
 
     return app
 
