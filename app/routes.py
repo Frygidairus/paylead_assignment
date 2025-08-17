a8359780c60ec6430e7e755144356619b81c7efe
Gwenn Durpoix-Espinasson <durpoixgwenn@gmail.com>
2025-08-17 12:39:46 +0200

    add GET route with filters

--- app/routes.py
+++ app/routes.py
@@ -14,3 +14,24 @@ def create_point_of_sales():
     db.session.commit()
 
     return jsonify({"id": pos.id, "store_name": pos.store_name}), 201
+
+@pos.route('/point_of_sales', methods=['GET'])
+def list_point_of_sales():
+
+    siret = request.args.get("siret")
+    zip_code = request.args.get("zip_code")
+    query = PointOfSale.query.filter_by(is_deleted=False)
+
+    if siret:
+        query = query.filter_by(siret=siret)
+
+    if zip_code:
+        query = query.filter_by(zip_code=zip_code)
+
+    #using list comprehension for better performance
+    result = [
+        {"id": p.id, "store_name": p.store_name, "siret": p.siret, "zip_code": p.zip_code}
+        for p in query.all()
+    ]
+
+    return jsonify(result)
